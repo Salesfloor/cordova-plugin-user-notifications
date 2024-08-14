7a6745c4a728e5cc9620c1769b350adf483e4407
Veaceslav Botnaru <veaceslav@salesfloor.net>
2024-08-14 14:25:04 -0400

    CPD-1566 Added functionality to remove all delivered notifications.

--- src/ios/CPUserNotifications.m
+++ src/ios/CPUserNotifications.m
@@ -4,10 +4,13 @@
 
 - (void)removeAllDelivered:(CDVInvokedUrlCommand*)command
 {
-    NSString* msg;
-    msg = [NSString stringWithFormat: @"%s", "removeAllDelivered executed"];
-
-    CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:msg];
+    //    NSString* msg;
+    //    msg = [NSString stringWithFormat: @"%s", "removeAllDelivered executed"];
+    //    CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:msg];
+    UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+    [center removeAllDeliveredNotifications];
+    
+    CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
     [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
 }
 
